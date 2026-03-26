70a34b887f598adcde02b0616d4c44fa349cf72b
Luccas Fialho <luccas.flh@gmail.com>
2026-03-25 21:40:53 -0300

    add contact functions to `utils.ts`

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,17 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+const phone = "41992475613";
+
+export function getWhatsAppLink(planName: string) {
+  const message = `Olá! Tudo bem? Vi o plano ${planName} no site e gostei bastante. Tenho interesse em me matricular, pode me orientar nos próximos passos?`;
+
+  return `https://wa.me/${phone}?text=${encodeURIComponent(message)}`;
+}
+
+export function getWhatsappNavbarLink() {
+  const message = `Olá, tudo bem? Tenho interesse em me matricular na academia e gostaria de saber os próximos passos.`;
+
+  return `https://wa.me/${phone}?text=${encodeURIComponent(message)}`;
+}
